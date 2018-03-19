af07f5e9299bebfe885ef9d161b4172acc0a3761
Catarina19 <Catarina.DE-JESUS@cpnv.ch>
2018-03-19 16:53:11 +0100

    démo et correction

--- code/vue/gabarit.php
+++ code/vue/gabarit.php
@@ -110,29 +110,6 @@
       </div>
     </div>
 
-    <div class="row-fluid">
-      <div class="span12">
-
-<!-- ________ SLIDER_____________-->
-<?php if((@$_GET['action']=="vue_accueil")||(!isset($_GET['action']))) :?>
-        <div id="headerSeparator"></div>
-
-        <div class="camera_full_width">
-          <div id="camera_wrap">
-            <div data-src="contenu/slider-images/5.jpg" ><div class="camera_caption fadeFromBottom cap1">Les derniers modèles toujours à disposition.</div></div>
-            <div data-src="contenu/slider-images/1.jpg" ><div class="camera_caption fadeFromBottom cap2">Découvrez des paysages fabuleux avec des sensations.</div></div>
-            <div data-src="contenu/slider-images/2.jpg" ></div>
-          </div>
-          <br style="clear:both"/><div style="margin-bottom:40px"></div>
-        </div>
-
-        <div id="headerSeparator2"></div>
-<!-- ________ SLIDER_____________-->
-<?php endif; ?>
-      </div>
-    </div>
-    </div>
-
     <div class="contentArea">
 
       <div class="divPanel notop page-content">
