a12a6bd3d5de2b94091a8f98359ecb468d4d856c
Catarina19 <Catarina.DE-JESUS@cpnv.ch>
2018-03-22 12:12:42 +0100

    thème graphique

--- code/vue/vue_profil.php
+++ code/vue/vue_profil.php
@@ -4,7 +4,16 @@ ob_start();
 $titre = 'Colocation - Login';
 ?>
     <div class="span12" id="divMain">
-        <h1>Profil</h1>
+        <h1 style="text-align: center;">Votre Profil</h1>
+
+        <div class="span5" id="profil_info">
+            Prenom Nom
+        </div>
+
+        <div class="span6" id="profil_appart">
+            à rajouter après
+        </div>
+
     </div>
 
 <?php
